6385983d8d250e24fe546ed73360f6488a2add48
Torjus Tveten <55440508+Torjustveten@users.noreply.github.com>
2020-05-28 15:02:02 +0200

    Update GameController.cs

--- NumberPuzzleWeb/NumberPuzzleWeb/Controllers/GameController.cs
+++ NumberPuzzleWeb/NumberPuzzleWeb/Controllers/GameController.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NumberPuzzleWeb.Core.ApplicationServices;
+using NumberPuzzleWeb.Core.DomainModel;
 
 namespace NumberPuzzleWeb.API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class GameController : Controller
     {
-        public IActionResult Index()
+        public GameController(GameService gameService)
         {
-            return View();
+            
+        }
+        [HttpGet]
+        public async Task<GameModel> StartGame()
+        {
+        }
+
+        [HttpGet("{gameId}")]
+        public async Task<GameModel> Read(Guid gameId)
+        {
+           
+        }
+
+        [HttpPost]
+        public async Task<GameModel> Play(int index, Guid gameId)
+        {
+            
         }
     }
 }
